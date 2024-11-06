9025c4f148099155d30e7c038d14d26eaae12ae1
shashinherath <shashin.malinda@gmail.com>
2024-11-06 19:05:00 +0530

    Create index.jsx

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import "../global.css";
+
+export default function index() {
+  return (
+    <View>
+      <Text className="text-blue-700 font-bold">Hello World!</Text>
+    </View>
+  );
+}
